2e2d4831b59754f85580c9622c9d337de8ad03b6
Adi <adinugraha9393@yahoo.com>
2019-01-25 18:07:14 +0700

    balance-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { HistoryComponent } from './history/history.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DepositComponent,
+    HistoryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
